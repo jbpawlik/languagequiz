e7b31e65437bc2bea853f2bf763b983c19f3cf60
Ben Pawlik <james.benjamin.pawlik@gmail.com>
2021-06-04 10:10:25 -0700

    add radio selector for interests

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,20 @@
 $(document).ready(function() {
+  $('form#interests').submit(function(event) {
+    event.preventDefault();
 
-const age = $select.val()
+  const interest = $('input:radio:checked[name=interests]').val()
 
-if (age === '1')
-  $('#age').change(function() {
-  $('age option:selected').text();
-  $('#child').show();
-  });
-})
+    if (interest === 'gaming') {
+      $('#interest').text('You might want to learn how to program with C#. C# is used in the popular game creation engine Unity.');
+    } else if (interest === 'websites') {
+      $('#interest').text('You might want to learn how to program in JavaScript. JavaScript is a language that is used in all websites. There are many useful libraries and tools that exist for users of Unity.');
+    } else if (interest === 'applications') {
+      $('#interest').text('You might want to learn how to program in Java. Java is not the same as JavaScript. It is an older, widely-used language that has extensive documentation and debugging tools.')
+    }
+    $('#interestOutput').show()
+  })  
+});
 
-// if (select = '1')
-//   $('#child').show(); {
-//     else if (val = '2');
-  
-//   };
-
-
-// $(document).ready(function() {
-//   $('form#superhero').submit(function(event) {
-//     event.preventDefault();
-
-//     const age = $('input:radio:checked[name=age]').val()
-
-//     if (age === 'minor') {
-//       $('#hero').text('Baby');
-//     } else if (age === 'adult') {
-//       $('#hero').text('Adult');
-//     } else if (age === 'senior') {
-//       $('#hero').text('Senior')
-//     }
-//     $('#whichsuperhero').show()
-//   })  
-// });
-  
+  // if ('#age option:selected')
+  // $('#child').show();
+  // })
