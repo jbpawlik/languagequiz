6eafc66a20dfaba620af49fc7410d3715860261c
Ben Pawlik <james.benjamin.pawlik@gmail.com>
2021-06-04 09:05:17 -0700

    add age selector

---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+
+const age = $('#age').val(1);
+  
+if (age = '1')
+  $('#child').show();
+
+})
+
+// $(document).ready(function() {
+//   $('#formOne').submit(function(event){
+//     event.preventDefault();
+//     const animalInput = $('input#animal').val();
+//     const animalsInput = $('input#animals').val();
+//     $('.animals').append(animalsInput);
+//     $('.animal').append(animalInput);
+    
+//     $('#story').show();
+//   })
+
+
+// })
+
+
+// $(document).ready(function() {
+//   $('form#superhero').submit(function(event) {
+//     event.preventDefault();
+
+//     const age = $('input:radio:checked[name=age]').val()
+
+//     if (age === 'minor') {
+//       $('#hero').text('Baby');
+//     } else if (age === 'adult') {
+//       $('#hero').text('Adult');
+//     } else if (age === 'senior') {
+//       $('#hero').text('Senior')
+//     }
+//     $('#whichsuperhero').show()
+//   })  
+// });
+  
