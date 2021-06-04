316bfa531515fdafe8b5a65394ebdb66e8f02128
Ben Pawlik <james.benjamin.pawlik@gmail.com>
2021-06-04 16:31:52 -0700

    update CSS styling

--- js/scripts.js
+++ js/scripts.js
@@ -16,9 +16,9 @@ $(document).ready(function() {
     const quiz2 = $('input:radio:checked[name=fun]').val()
     const quiz3 = $('input:radio:checked[name=hero]').val()
 
-    if (quiz1 === 'hot') {
+    if (quiz1 === 'cold') {
       (sum1 = 1);
-    } else if (quiz1 === 'cold') {
+    } else if (quiz1 === 'hot') {
       (sum1 = 2);
     } else if (quiz1 === 'cool') {
       (sum1 = 3);
@@ -41,9 +41,33 @@ $(document).ready(function() {
     }
 
     const perResult = add(sum1, sum2, sum3);
-    alert(perResult);
 
-  })
+    if (perResult === 3) {
+      $('#perLang').text("binary. You are obviously a computer.").toggle;
+      $('#outputLang').show();
+    } else if (perResult === 4) {
+      $('#perLang').text("Perl. You are obviously a clam.").toggle;
+      $('#outputLang').show();
+    } else if (perResult === 5) {
+      $('#perLang').text("Python. You are obviously a snake.").toggle;
+      $('#outputLang').show(); 
+    } else if (perResult === 6) {
+      $('#perLang').text("C#. You are obviously a musician.").toggle;
+      $('#outputLang').show(); 
+    } else if (perResult === 6) {
+      $('#perLang').text("C. You are obviously a wave.").toggle;
+      $('#outputLang').show(); 
+    } else if (perResult === 7) {
+      $('#perLang').text("Vue. You are obviously a giant eyeball.").toggle;
+      $('#outputLang').show(); 
+    } else if (perResult === 8) {
+      $('#perLang').text("React. You are obviously a hothead.").toggle;
+      $('#outputLang').show(); 
+    } else if (perResult === 9) {
+      $('#perLang').text("Delphi. You are obviously Marina Abramovic.").toggle;
+      $('#outputLang').show(); 
+    }
+    })
   });
 
 $(document).ready(function() {
@@ -53,13 +77,13 @@ $(document).ready(function() {
   const interest = $('input:radio:checked[name=interests]').val()
 
     if (interest === 'gaming') {
-      $('#interest').text('You might want to learn how to program with C#. C# is used in the popular game creation engine Unity.');
+      $('#interest').text('You might want to learn how to program with C#. C# is used in the popular game creation engine Unity.').toggle;
     } else if (interest === 'websites') {
-      $('#interest').text('You might want to learn how to program in JavaScript. JavaScript is a language that is used in all websites. There are many useful libraries and tools that exist for users of Unity.');
+      $('#interest').text('You might want to learn how to program in JavaScript. JavaScript is a language that is used in all websites. There are many useful libraries and tools that exist for users of Unity.').toggle;
     } else if (interest === 'applications') {
-      $('#interest').text('You might want to learn how to program in Java. Java is not the same as JavaScript. It is an older, widely-used language that has extensive documentation and debugging tools.')
+      $('#interest').text('You might want to learn how to program in Java. Java is not the same as JavaScript. It is an older, widely-used language that has extensive documentation and debugging tools.').toggle;
     }
-    $('#interestOutput').show()
+    $('#interestOutput').show();
   })  
 });
 
